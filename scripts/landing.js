3f9ed8775d75f8522604e57c26877c82d9cd4c1a
PatriciaEhrhardt <pinkopaque22@gmail.com>
2016-08-13 14:53:26 -0700

    Make selling points selector global

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,5 @@
-var animatePoints = function() {
-
-	var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
+var animatePoints = function(points) {
 	var revealPoint = function(index) {
 			points[index].style.opacity = 1;
 			points[index].style.transform = "scaleX(1) translateY(0)";

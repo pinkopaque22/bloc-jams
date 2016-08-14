8eebfebb527ddfdb4772f59819c75388a31ac27e
PatriciaEhrhardt <pinkopaque22@gmail.com>
2016-08-14 13:53:39 -0700

    Add toggle feature via an array of albums

--- scripts/album.js
+++ scripts/album.js
@@ -1,3 +1,25 @@
+var albumTwilightSingers = {
+	title: 'Powder Burns',
+	artist: 'The Twilight Singers',
+	label: 'One Little Indian Records',
+	year: '2006',
+	albumArtUrl: 'assets/images/album_covers/dulli.jpeg',
+	songs: [
+		{title: 'Toward The Waves', duration: '0:29' },
+		{title: 'I\n m Ready', duration: '3:05' },
+		{title: 'There\n s Been an Accident', duration: '5:17' },
+		{title: 'Bonnie Brae', duration: '4:46' },
+		{title: 'Forty Dollars', duration: '3:51' },
+		{title: 'Candy Cane Crawl', duration: '4:27' },
+		{title: 'Underneath the Waves', duration: '4:29' },
+		{title: 'My Time (Has Come)', duration: '4:23' },
+		{title: 'Dead to Rights', duration: '4:25' },
+		{title: 'The Conversation', duration: '3:05' },
+		{title: 'Powder Burns', duration: '5:53' },
+		{title: 'I Wish I Was', duration: '4:21' }
+	]
+};
+
 var albumPicasso = {
 	title: 'The Colours',
 	artist: 'Pablo Picasso',
@@ -38,12 +60,14 @@ var createSongRow = function(songNumber, songName, songLength) {
 	
 	return template;
 };
+var albumTitle = document.getElementsByClassName('album-view-title') [0];
+var albumArtist = document.getElementsByClassName('album-view-artist') [0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info') [0];
+var albumImage = document.getElementsByClassName('album-cover-art') [0];
+var albumSongList = document.getElementsByClassName('album-view-song-list') [0];
+
 var setCurrentAlbum = function(album) {
-	var albumTitle = document.getElementsByClassName('album-view-title') [0];
-	var albumArtist = document.getElementsByClassName('album-view-artist') [0];
-	var albumReleaseInfo = document.getElementsByClassName('album-view-release-info') [0];
-	var albumImage = document.getElementsByClassName('album-cover-art') [0];
-	var albumSongList = document.getElementsByClassName('album-view-song-list') [0];
+	
 	
 	
 	albumTitle.firstChild.nodeValue = album.title;
@@ -60,4 +84,16 @@ var setCurrentAlbum = function(album) {
 
 window.onload = function() {
 	setCurrentAlbum(albumPicasso);
+	
+	var albums = [albumTwilightSingers, albumPicasso, albumMarconi ];
+	var index = 0;
+	albumImage.addEventListener("click", function(event) {
+		setCurrentAlbum(albums[index]);
+		index++;
+		if (index == albums.length) {
+			index = 0;
+		}
+		
+															
+	});
 };

ea6d06205be4b11a7318ae5662271068e0343c3e
PatriciaEhrhardt <pinkopaque22@gmail.com>
2016-08-14 12:56:14 -0700

    Checkpoint complete

--- scripts/album.js
+++ scripts/album.js
@@ -41,7 +41,7 @@ var createSongRow = function(songNumber, songName, songLength) {
 var setCurrentAlbum = function(album) {
 	var albumTitle = document.getElementsByClassName('album-view-title') [0];
 	var albumArtist = document.getElementsByClassName('album-view-artist') [0];
-	var albumReleaseInfo= document.getElementsByClassName('album-view-release-info') [0];
+	var albumReleaseInfo = document.getElementsByClassName('album-view-release-info') [0];
 	var albumImage = document.getElementsByClassName('album-cover-art') [0];
 	var albumSongList = document.getElementsByClassName('album-view-song-list') [0];
 	
@@ -53,7 +53,7 @@ var setCurrentAlbum = function(album) {
 	
 	albumSongList.innerHTML = '';
 	
-	for (var i = 0; i <album.songs.length; i++) {
+	for (var i = 0; i < album.songs.length; i++) {
 		albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
 	}
 };

8aa0e1f9b99b30d02cbbb9789c2ffeaab48c9d10
PatriciaEhrhardt <pinkopaque22@gmail.com>
2016-08-21 14:45:09 -0700

    Delegate event listener to track mouse position.

--- scripts/album.js
+++ scripts/album.js
@@ -35,7 +35,7 @@ var createSongRow = function(songNumber, songName, songLength) {
 	+' <td class="song-item-duration">' + songLength + '</td>'
 	+'</tr>'
 	;
-	
+
 	return template;
 };
 var setCurrentAlbum = function(album) {
@@ -44,20 +44,26 @@ var setCurrentAlbum = function(album) {
 	var albumReleaseInfo = document.getElementsByClassName('album-view-release-info') [0];
 	var albumImage = document.getElementsByClassName('album-cover-art') [0];
 	var albumSongList = document.getElementsByClassName('album-view-song-list') [0];
-	
-	
+
+
 	albumTitle.firstChild.nodeValue = album.title;
 	albumArtist.firstChild.nodeValue = album.artist;
 	albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
 	albumImage.setAttribute('src', album.albumArtUrl);
-	
+
 	albumSongList.innerHTML = '';
-	
+
 	for (var i = 0; i < album.songs.length; i++) {
 		albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
 	}
 };
+var songListContainer = document.getElementsByClassName('album-view-song-list') [0];
+
 
 window.onload = function() {
 	setCurrentAlbum(albumPicasso);
+
+	songListContainer.addEventListener('mouseover', function(event) {
+		  console.log(event.target);
+	});
 };

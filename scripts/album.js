128b8d12f7e792d23eaa5b3f6dce0dc68d32a66e
PatriciaEhrhardt <pinkopaque22@gmail.com>
2016-08-21 15:53:29 -0700

    Add play button in place of song number

--- scripts/album.js
+++ scripts/album.js
@@ -59,11 +59,16 @@ var setCurrentAlbum = function(album) {
 };
 var songListContainer = document.getElementsByClassName('album-view-song-list') [0];
 
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-pizza"></span></a>'
+
 
 window.onload = function() {
 	setCurrentAlbum(albumPicasso);
 
 	songListContainer.addEventListener('mouseover', function(event) {
-		  console.log(event.target);
+		  if(event.target.parentElement.className === 'album-view-song-item') {
+				event.target.parentElement.querySelector('.song-item-numner').innerHTML = playButtonTemplate;
+			}
+		
 	});
 };
